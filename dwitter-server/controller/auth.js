e9f1c43eb700843a9b69be5d36e4c2225c634a48
jinjin <jinjinx28@gmail.com>
2026-05-14 12:02:23 +0900

    2026-05-14 commit

--- dwitter-server/controller/auth.js
+++ dwitter-server/controller/auth.js
@@ -1,34 +1,66 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import * as repository from "../repository/signUp.js";
+import dotenv from 'dotenv';
+import * as repository from "../repository/auth.js";
 
-const SECRET = 'YOUR_SECRET_KEY'; 
+dotenv.config();
 
-export const getAuth = async (req, res) => {
+/**
+ * 로그인
+ */
+export const getLogin = async (req, res) => {
   const { username, password } = req.body;
+  
   try {
     const user = await repository.getLogin(username);
-    const valid = user.count && await bcrypt.compare(password, user.password);
-
-    if (!user.count || !valid) {
+    const valid = await bcrypt.compare(password, user.password);
+console.log(user, valid, !user.count, !valid);
+    
+    if(!user.count || !valid) {
       return res.status(401).json({ message: '아이디 또는 비밀번호가 틀렸습니다.' });
-    }
+    } 
+    
+    const token = jwt.sign({ 
+      id: user.id, 
+      username: user.username 
+    }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    
+    // console.log(token, user);
+        
+    res.json({  token, 
+                user: { id: user.id, 
+                        username: user.username, 
+                        profileImage: user.avatar_url 
+              } });
+  } catch {
+    res.status(500).json({ message: '서버 오류' });
+  }
+}
 
-    const token = jwt.sign({
-      id: user.id,
-      username: user.username
-    }, SECRET, { expiresIn: '7d' });
 
-    res.json({
-      token,
-      user: {
-        id: user.id,
-        username: user.username,
-        profileImage: user.avatar_url
-      }
+/**
+ * 회원가입
+ */
+export const getAuth = async (req, res) => {
+  const SECRET = process.env.JWT_SECRET; // .env에서 불러오기
+  const { userName, password, profileImage } = req.body;
+
+  try {
+    //패스워드 암호화
+    const hashed = await bcrypt.hash(req.body.password, 10);
+      const result = await repository.signUp({
+      userName,
+      password: hashed,
+      profileImage,
     });
-  } catch (error) {
-    console.error('🔥 로그인 에러 상세:', error);
-    res.status(500).json({ message: '서버 에러' });
+
+    const token = jwt.sign({ id: result.insertId, userName }, SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(201).json({ token, user: { id: result.insertId, userName } });
+  } catch (err) {
+    if (err.code === "ER_DUP_ENTRY")
+      return res.status(409).json({ message: "이미 사용 중인 아이디입니다." });
+    res.status(500).json({ message: "서버 오류" });
   }
 };
